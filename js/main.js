e21487cfc0a0fddfe267c310dcc5ca78a5e10e12
Kiiwi <tommy.lee.ryan@gmail.com>
2014-03-13 03:16:58 +0100

    removed jquery-ui-slider-pips, added jqueryUI sliders

--- js/main.js
+++ js/main.js
@@ -6,3 +6,36 @@ $("#hintbutton").click(function () {
 $("#settings").click(function () {
     $(".sliders").slideToggle("slow");
 });
+
+//TODO FIX SLIDER VALUE BUG
+$(function () {
+    $("#monthSlider").slider({
+        range: true,
+        min: 1,
+        max: 12,
+        values: [ 1, 12],
+        slide: function (event, ui) {
+            $("#monthAmount").val(ui.values[0] + ui.values[ 1 ]);
+        }
+    });
+    $("#monthAmount").val($("#monthSlider").slider("values", 0) +
+        " - " + $("#monthSlider").slider("values", 1));
+
+});
+
+$(function () {
+    $("#yearSlider").slider({
+        range: true,
+        min: 1970,
+        max: 2014,
+        values: [ 1970, 2014],
+        slide: function (event, ui) {
+            $("#yearAmount").val(ui.values[0] + ui.values[ 1 ]);
+        }
+    });
+    $("#yearAmount").val($("#yearSlider").slider("values", 0) +
+        " - " + $("#yearSlider").slider("values", 1));
+});
+
+
+
